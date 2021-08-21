c91225fc55bf2d08420b682a9293b34533e3d426
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2021-08-20 17:26:36 -0700

    Fix bug, replace NPS-generation with bespoke-deepgen

--- python/util/split_hmdb.py
+++ python/util/split_hmdb.py
@@ -14,12 +14,12 @@ from itertools import chain
 from rdkit import Chem
 from tqdm import tqdm
 
-if os.path.isdir("~/git/NPS-generation"):
-    git_dir = os.path.expanduser("~/git/NPS-generation")
+if os.path.isdir("~/git/bespoke-deepgen"):
+    git_dir = os.path.expanduser("~/git/bespoke-deepgen")
 elif os.path.isdir("/Users/gregstacey/Academics/Foster/Metabolomics/bespoke-deepgen"):
     git_dir = os.path.expanduser("~/Academics/Foster/Metabolomics/bespoke-deepgen")
-elif os.path.isdir("/scratch/st-ljfoster-1/staceyri/NPS-generation"):
-    git_dir = os.path.expanduser("/scratch/st-ljfoster-1/staceyri/NPS-generation")
+elif os.path.isdir("/scratch/st-ljfoster-1/staceyri/bespoke-deepgen"):
+    git_dir = os.path.expanduser("/scratch/st-ljfoster-1/staceyri/bespoke-deepgen")
 python_dir = git_dir + "/python"
 os.chdir(python_dir)
 sys.path.append(python_dir)
