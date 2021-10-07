f030ee0b989c15d7cb7df7d9458eb01bf7e5f4c9
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2021-10-07 15:43:35 -0700

    Put sample_idx in train_model.py scheduling script

--- R/outer_train.R
+++ R/outer_train.R
@@ -25,6 +25,10 @@ fns = dir("data/hmdb/", pattern = "_clean.smi", full.names = T, recursive = T) %
 # just superklasses + kingdoms for now
 fns = fns[grepl("superklass", fns) | grepl("kingdom", fns)]
 
+# only .smi files with >100 samples
+nsample = sapply(fns, function(x) read_csv(x) %>% nrow())
+fns = fns[nsample>135]
+
 # set up grid
 jobs = tidyr::crossing(enum = 0,
                        n_layers = 3,
@@ -38,11 +42,12 @@ jobs = tidyr::crossing(enum = 0,
                        smiles_file = fns) %>%
   mutate(output_dir = paste0(project_dir, "experiments/01_kategory/",
                              basename(smiles_file) %>% gsub("_clean.smi", "", .), "/")) %>%
+  mutate(generated_samples = paste0(output_dir, "sample-", sample_idx+1,"-SMILES.smi")) %>%
+  mutate(model_file = paste0(output_dir, "model-", sample_idx+1,".pt")) %>%
   # # vocab file
   # mutate(fn_vocab = paste0("experiments/01_kategory/",
   #                          file_path_sans_ext(basename(smiles_file)))) %>%
-  # filter jobs that exist
-  mutate(model_file = paste0(output_dir, "sample-", sample_idx+1,"-SMILES.smi")) %>%
+  # filter out jobs that exist
   filter(!file.exists(model_file)) %>%
   as.data.frame()
 
@@ -50,7 +55,6 @@ jobs = tidyr::crossing(enum = 0,
 if (this.system=="sockeye") {
   jobs = jobs[sample(nrow(jobs), 49),]
 }
-
 
 # make output dirs if needed
 for (ii in 1:length(unique(jobs$output_dir))) {
@@ -86,3 +90,27 @@ write_sh(job_name = job_name,
 # finally, run the job on whatever system we're on
 args = data.frame(allocation = "st-ljfoster-1-gpu")
 submit_job(nrow(jobs), sh_file, args$allocation, this.system)
+
+
+
+module load Software_Collection/2021
+module load gcc/9.4.0 openmpi python singularity
+
+LINE_IDX=$((PBS_ARRAY_INDEX + 1))
+LINE=`sed "${LINE_IDX}q;d" /scratch/st-ljfoster-1/staceyri/bespoke-deepgen/sh/grids/train.txt`
+IFS=$'\t' PARAMS=($LINE)
+AUGMENTATION=${PARAMS[0]}
+N_LAYERS=${PARAMS[1]}
+EMB_SIZE=${PARAMS[2]}
+HIDDEN_SIZE=${PARAMS[3]}
+RNN_TYPE=${PARAMS[4]}
+DROPOUT=${PARAMS[5]}
+BATCH_SIZE=${PARAMS[6]}
+LEARNING_RATE=${PARAMS[7]}
+SAMPLE_IDX=${PARAMS[8]}
+SMILES_FILE=${PARAMS[9]}
+OUTPUT_DIR=${PARAMS[10]}
+
+cd /scratch/st-ljfoster-1/staceyri/bespoke-deepgen/python
+
+singularity exec --nv /arc/project/st-ljfoster-1/Conda_Container.sif python3 train_model.py --smiles_file $SMILES_FILE --output_dir $OUTPUT_DIR --rnn_type $RNN_TYPE --hidden_size $HIDDEN_SIZE --embedding_size $EMB_SIZE --n_layers $N_LAYERS --dropout $DROPOUT --learning_rate $LEARNING_RATE --batch_size $BATCH_SIZE --sample_size 500000 --log_every_steps 100 --max_epochs 999999 --patience 50000 --stop_if_exists
